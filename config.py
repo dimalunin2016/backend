3c0379bac1b902db0b5c4f4ad7f9d201092ce416
DmitryLunin <dimalynin@gmail.com>
2019-04-03 05:22:47 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'strong-password'
+    SECURITY_PASSWORD_SALT = 'my_precious_two'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # mail settings
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+
+    # gmail authentication
+    MAIL_USERNAME = os.environ['APP_MAIL_USERNAME']
+    MAIL_PASSWORD = os.environ['APP_MAIL_PASSWORD']
+
+    # mail accounts
+    MAIL_DEFAULT_SENDER = 'dimalynin@gmail.com'
+
+
